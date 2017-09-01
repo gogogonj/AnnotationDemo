d909a5b4212bbe71e0e1fd0748e82ff0a3aacf62
wangwei_yx <wangwei@asiainfo.com>
2017-09-01 16:02:57 +0800

    首次提交

---
+++ src/main/java/annotation/Test.java
@@ -0,0 +1,12 @@
+package annotation;
+
+/**
+ * Created by AI on 2017/9/1.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        System.out.println(apple.getAppleName() + "" + apple.getAppleColor());
+    }
+}
